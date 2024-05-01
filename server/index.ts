6b1c3aedc3d94265e52967dbccc047262a1bd7f6
tadjh <tadjh.brooks@gmail.com>
2024-05-01 13:06:26 -0700

    Remove server boilerplate

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,1 @@
-import { myRandomData } from "./MyOther.server";
-
-// on("onResourceStart", (resName: string) => {
-//   if (resName === GetCurrentResourceName()) {
-//     console.log("TypeScript boilerplate started!");
-//     console.log(myRandomData);
-//   }
-// });
+// Cache user input
